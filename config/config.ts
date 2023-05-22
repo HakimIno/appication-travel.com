ba3d766265c6c4703ac6b9677660bc6d51cc13a1
weerachit <keemkeem207@gmail.com>
2023-05-22 19:37:59 +0700

    feat: travel-app success

--- config/config.ts
+++ config/config.ts
@@ -5,13 +5,7 @@ import { getStorage } from "firebase/storage";
 
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyDVE8vqdxMruFlgvN0JtOv1c6qaohw0v90",
-    authDomain: "travelapp12-2eddc.firebaseapp.com",
-    projectId: "travelapp12-2eddc",
-    storageBucket: "travelapp12-2eddc.appspot.com",
-    messagingSenderId: "1090135174394",
-    appId: "1:1090135174394:web:772f7f6f487bbc918ead43",
-    measurementId: "G-J77T34T9PX"
+    
 }
 
 
